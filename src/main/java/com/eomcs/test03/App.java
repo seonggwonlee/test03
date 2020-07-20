dd330fdf45bc5f0b7e6a7e7fb650cc125976f195
seoggwonlee <sysktho@gmail.com>
2020-07-20 19:35:42 +0900

    test03 백업

--- src/main/java/com/eomcs/test03/App.java
+++ src/main/java/com/eomcs/test03/App.java
@@ -12,5 +12,6 @@ public class App {
         System.out.println(new App().getGreeting());
         System.out.println(new App().getGreeting());
         System.out.println(new App().getGreeting());
+        System.out.println(new App().getGreeting());
     }
 }
